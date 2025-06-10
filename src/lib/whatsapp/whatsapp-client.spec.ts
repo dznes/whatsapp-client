62c916214617f33f93cf036f5f4ccfb030bcc2cf
dznes <davidkim113@gmail.com>
2025-06-10 18:24:12 -0300

    add test using top level Whatsapp API client class

---
+++ src/lib/whatsapp/whatsapp-client.spec.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect } from "vitest"
+import { WhatsappClient } from "."
+import { env } from "../../env";
+
+const numberId = env.WA_PHONE_NUMBER_ID;
+const version = env.CLOUD_API_VERSION;
+const token = env.CLOUD_API_ACCESS_TOKEN;
+const recipient_number = "5511968622121"
+
+describe("Whatsapp Client tests", () => {
+  it("Should be able to init Whatsapp client", () => {
+    const client = new WhatsappClient(token, version, numberId)
+    expect(client).toBeTruthy()
+  })
+  it("Should be able to use Message methods", async () => {
+    const client = new WhatsappClient(token, version, numberId)
+    const data = await client.messages.send({
+      type: "text",
+      recipient_number,
+      content: "Envio teste utilizando Whatsapp Client",
+    })
+    expect(data.messages[0].id).toBeTruthy()
+  })
+})
